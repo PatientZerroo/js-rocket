73f6882f3a2ab719f3108beebeb9f066dc185c70
ZenexGaming <40682066+ZenexGaming@users.noreply.github.com>
2018-06-30 11:16:45 -1000

    Added 'doCoolStuff' function and transition styles

--- js/script.js
+++ js/script.js
@@ -1,7 +1,22 @@
+// (>")>  <("<)(>")>  <("<)(>")>  <("<)(>")>  <("<) *Kerby Dancing Bitch*
+
+var doCoolStuff = function () {
+	var currentClassName = document.getElementById('cool').className;
+
+	if (currentClassName == 'cool') {
+		document.getElementById('cool').className = 'cool red';
+	}else {
+		document.getElementById('cool').className = 'cool';
+	}
+
+	//Changing ClassName
+	//document.getElementById('cool').className = 'cool red';
+}
+
 var sayMyName = function (name) {
 	alert('My Name is: ' +name);
 }
-
+// This variable identifies the 'Car' with functions
 var car = {
 		make: 'BMW',
 		type: 'Sport',
